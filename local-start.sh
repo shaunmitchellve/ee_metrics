bfba6d69f4311b41c65256f515b02f1f43bd1871
Shaun Mitchell <shaun@reddigitalio.com>
2023-05-04 14:07:48 -0600

    Update local-start.sh

--- local-start.sh
+++ local-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export FUNCTION_TARGET=ReadTimeSeriesFields
-export PROJECTID=ce-monitoring
+export PROJECTID=<GCP_PROJECT_ID>
 export TABLEID=cpu_usage_time
 export DATASETID=earth_engine_metrics
 export TIMEWINDOW=30m
