2903c3d2dcb41b208197e76d3085f0302c5b7cdc
Shaun Mitchell <shaun@reddigitalio.com>
2023-05-04 14:07:10 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+PROJECT_ID=<GCP-PROJECT-ID>
+PUBSUB_TRIGGER=<TRIGGER-NAME>
+SA=<SERVICE-ACCOUNT>
+
+
 gcloud functions deploy ee-collect-metrics \
 --region=us-central1 \
 --gen2 \
---project=ce-monitoring \
+--project=${PROJECT_ID} \
 --runtime=go119 \
 --source=. \
 --entry-point=ReadTimeSeriesFields \
---trigger-topic=start-ee-metric-collection \
+--trigger-topic=${PUBSUB_TRIGGER} \
 --region=us-central1 \
---service-account=ee-collect-metrics-runner@ce-monitoring.iam.gserviceaccount.com \
---set-env-vars=FUNCTION_TARGET=ReadTimeSeriesFields,PROJECTID=ce-monitoring,TABLEID=cpu_usage_time,DATASETID=earth_engine_metrics,TIMEWINDOW=30m,AGGREGATION=1800 \
+--service-account=${SA} \
+--set-env-vars=FUNCTION_TARGET=ReadTimeSeriesFields,PROJECTID=${PROJECT_ID},TABLEID=cpu_usage_time,DATASETID=earth_engine_metrics,TIMEWINDOW=30m,AGGREGATION=1800 \
 --timeout=90 \
 --memory=256MB \
 --ingress-settings=internal-only
